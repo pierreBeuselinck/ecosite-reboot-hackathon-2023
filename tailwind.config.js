0fcd945f92786e4b95fafe60a839baae8659a0e0
Matthys <mboureau59@gmail.com>
2023-11-14 21:42:58 +0100

    footer add

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./assets/composant/header.html"],
+  content: [
+    "./assets/composant/*.html", // Fichiers HTML dans le dossier components
+    "./*.html", // Fichiers HTML à la racine du projet
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
 
