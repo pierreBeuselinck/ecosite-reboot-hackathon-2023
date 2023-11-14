586dc65fe895a34ec5df4cd75b3e75aa98d761b7
Matthys <mboureau59@gmail.com>
2023-11-14 21:15:38 +0100

    header

--- postcss.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    syntax: 'postcss-scss',
-    plugins: [
-      require('tailwindcss'),
-      require('autoprefixer'),
-    ],
-  }
-  
--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./assets/composant/header.html"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
