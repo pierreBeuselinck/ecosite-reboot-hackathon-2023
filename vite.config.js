22f340b4d3b5d4462887d07eda38c533ccfac9cd
Pierre-Beuselinck <pierrebeuselinck2@gmail.com>
2023-11-15 13:53:55 +0100

    ajout des pages

--- vite.config.js
+++ vite.config.js
@@ -14,6 +14,16 @@ export default defineConfig({
     port: 3000,
   },
   build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html'),
+        ficheProduit: path.resolve(__dirname, 'fiche-produit.html'),
+        livraison: path.resolve(__dirname, 'livraison.html'),
+        paiement: path.resolve(__dirname, 'paiement.html'),
+        confirmation: path.resolve(__dirname, 'confirmation.html'),
+        classement: path.resolve(__dirname, 'classement.html'),
+      }
+    },
     outDir: 'dist',
     assetsDir: 'assets',
   },
