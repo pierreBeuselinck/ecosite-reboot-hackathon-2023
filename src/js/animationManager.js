034f1988fedd8b9b6937ee5ec0938afdd993775e
Pierre-Beuselinck <pierrebeuselinck2@gmail.com>
2023-11-16 17:06:49 +0100

    fix bugs

--- src/js/animationManager.js
+++ src/js/animationManager.js
@@ -1,39 +1,35 @@
-
-
-function animationInit() {
-
-  // FP : Title
+function setupTitleHangAnimation() {
   document.querySelectorAll('.click-hang').forEach(element => {
-    element.addEventListener('click', function () {
+    element.addEventListener('click', function() {
       this.classList.add('click-hang-animate');
     });
   });
+}
 
-  //FP : Title falldown
+function setupTitleFalldownAnimation() {
   document.querySelectorAll('.click-falldown').forEach(triggerElement => {
-    triggerElement.addEventListener('click', function () {
+    triggerElement.addEventListener('click', function() {
       document.querySelectorAll('.click-falldown-target').forEach(targetElement => {
         targetElement.classList.add('click-falldown-animation');
       });
     });
   });
+}
 
-  //FP: Avis falldown 
-
+function setupAvisFalldownAnimation() {
   document.querySelectorAll('.click-falldown-faq').forEach(element => {
-    element.addEventListener('click', function () {
+    element.addEventListener('click', function() {
       this.classList.add('click-falldown-faq-animation');
     });
   });
+}
 
-  //FP : Parapgraphe Falldown
-
+function setupParagrapheFalldownAnimation() {
   document.querySelectorAll('.click-letterFalldown').forEach(button => {
-    button.addEventListener('click', function () {
+    button.addEventListener('click', function() {
       document.querySelectorAll('.click-letterFalldown-target').forEach(textElement => {
         const text = textElement.textContent;
         textElement.innerHTML = '';
-
         const length = text.length;
         for (let i = 0; i < length; i++) {
           let span = document.createElement('span');
@@ -41,7 +37,6 @@ function animationInit() {
           span.style.animationDelay = `${(length - i) * 0.05}s`;
           textElement.appendChild(span);
         }
-
         setTimeout(() => {
           textElement.querySelectorAll('span').forEach(span => {
             span.classList.add('click-letter-falldown-animation');
@@ -50,185 +45,186 @@ function animationInit() {
       });
     });
   });
+}
 
-
-  //FP : changement de l'image au clique
-
+function setupImageSwitchOnClick() {
   document.querySelectorAll('.click-switchImg').forEach(button => {
-    button.addEventListener('click', function () {
-      const imageContainer = document.querySelector('.c-product-page__visual-wrapper');
-      const oldImage = imageContainer.querySelector('.click-switchImg-animation');
-
-      const newImage = document.createElement('img');
-      newImage.src = 'images/sapin-sans-epine.png';
-      newImage.alt = 'Image';
-      newImage.classList.add('click-switchImg-animation', 'click-switchImg-hide');
-
-      imageContainer.appendChild(newImage);
-
-      setTimeout(() => {
-        oldImage.classList.add('click-switchImg-hide');
-        newImage.classList.remove('click-switchImg-hide');
-      }, 100);
-
-      oldImage.addEventListener('transitionend', () => {
-        oldImage.remove();
+    button.addEventListener('click', function() {
+      const imageContainers = document.querySelectorAll('.c-product-page__visual-wrapper');
+      imageContainers.forEach(imageContainer => {
+        const oldImage = imageContainer.querySelector('.click-switchImg-animation');
+        if (oldImage) {
+          const newImage = document.createElement('img');
+          newImage.src = 'images/sapin-sans-epine.png';
+          newImage.alt = 'Image';
+          newImage.classList.add('click-switchImg-animation', 'click-switchImg-hide');
+          imageContainer.appendChild(newImage);
+          setTimeout(() => {
+            oldImage.classList.add('click-switchImg-hide');
+            newImage.classList.remove('click-switchImg-hide');
+          }, 100);
+          oldImage.addEventListener('transitionend', () => {
+            oldImage.remove();
+          });
+        }
       });
     });
   });
+}
 
-  // Paiement : click sur camion
-  var sidebar = document.getElementById("animSidebar");
-  var camionPaiement = document.querySelectorAll(".barre-avancement-camion");
-  camionPaiement.forEach(element => {
-    element.addEventListener('click', function () {
-      this.classList.add('anim-rotate');
-      sidebar.classList.add('anim-rotate-bar');
+function setupHeaderButtonExplosion() {
+  const explosionTrigger = document.querySelector('.click-headerCart-explosion-trigger');
+  if (explosionTrigger) {
+    explosionTrigger.addEventListener('click', function() {
+      this.classList.add('click-headerCart-explosion');
     });
-  });
-
-  // Paiement : click choix
-  var btnRadio1 = document.getElementById('radio1');
-  var btnRadio2 = document.getElementById('radio2');
-  var btnRadio3 = document.getElementById('radio3');
-  var ParentbtnRadio1 = document.getElementById('btn1');
-  var ParentbtnRadio2 = document.getElementById('btn2');
-  var ParentbtnRadio3 = document.getElementById('btn3');
-  
-  // Fonction pour retirer la classe 'inputChecked' de tous les éléments parents
-  function removeInputCheckedClassFromAll() {
-      ParentbtnRadio1.classList.remove('inputChecked');
-      ParentbtnRadio2.classList.remove('inputChecked');
-      ParentbtnRadio3.classList.remove('inputChecked');
   }
-  
-  // Fonction pour mettre à jour la classe de l'élément parent selon l'état du bouton radio
-  function updateParentClass(btnRadio, parentElement) {
-      if (btnRadio.checked) {
-          removeInputCheckedClassFromAll(); // Retire la classe de tous les éléments parents
-          parentElement.classList.add('inputChecked');
-      }
-  }
-  
-  // Ajout d'écouteurs d'événements de type 'change' pour les boutons radio
-  btnRadio1.addEventListener('change', function() {
-      updateParentClass(btnRadio1, ParentbtnRadio1);
-  });
-  
-  btnRadio2.addEventListener('change', function() {
-      updateParentClass(btnRadio2, ParentbtnRadio2);
-  });
-  
-  btnRadio3.addEventListener('change', function() {
-      updateParentClass(btnRadio3, ParentbtnRadio3);
-  });
-  
-  // Au chargement initial de la page, vérifier et appliquer les classes correctement
-  updateParentClass(btnRadio1, ParentbtnRadio1);
-  updateParentClass(btnRadio2, ParentbtnRadio2);
-  updateParentClass(btnRadio3, ParentbtnRadio3);
-  
+}
 
-
-  //Header : bouton explosion 
-
-  document.querySelector('.click-headerCart-explosion-trigger').addEventListener('click', function () {
-    this.classList.add('click-headerCart-explosion');
-  });
-
-  // Homepage : étoile de la cover
-
+function setupShootingStarsAnimation() {
   document.querySelectorAll('.shooting-stars').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
+    anchor.addEventListener('click', function(e) {
       e.preventDefault();
-
       const targetId = this.getAttribute('href');
       const targetElement = document.querySelector(targetId);
-
       if (targetElement) {
         targetElement.scrollIntoView({ behavior: 'smooth' });
-
-        // Déclencher l'animation après le défilement
         setTimeout(() => {
           targetElement.classList.add('animate-stars');
-        }, 500); // Ajustez ce délai en fonction du temps de défilement
+        }, 500);
       }
     });
   });
+}
 
-
-  //Homepage : product card hang btn
-
-
+function setupProductCardHangAnimation() {
   document.querySelectorAll('.hp-product-cards-hang-btn').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
+    anchor.addEventListener('click', function(e) {
       e.preventDefault();
-
       const targetId = this.getAttribute('href');
       const targetElement = document.querySelector(targetId);
-
       if (targetElement) {
         targetElement.scrollIntoView({ behavior: 'smooth' });
-
-        // Déclenchez l'animation après le défilement
         setTimeout(() => {
           document.querySelectorAll('.dp-card .dp-title button').forEach(button => {
             button.classList.add('hp-product-cards-hang-btn-animation');
           });
-        }, 500); // Ce délai devrait être ajusté en fonction de la durée du défilement
+        }, 500);
       }
     });
   });
+}
 
-  //HP : changement de l'image au clique
-
-  document.querySelector('.hp-click-switchImg').addEventListener('click', function () {
-    document.querySelectorAll('.dp-card-img-wrapper').forEach(container => {
-      const oldImage = container.querySelector('.hp-click-switchImg-animation');
-
-      const newImage = document.createElement('img');
-      newImage.src = 'images/sapin-sans-epine.png'; // Mettez à jour avec le nouveau chemin d'image
-      newImage.alt = 'Nouvelle Image';
-      newImage.classList.add('hp-click-switchImg-animation', 'hp-click-switchImg-hide');
-
-      container.appendChild(newImage);
-
-      setTimeout(() => {
-        oldImage.classList.add('hp-click-switchImg-hide');
-        newImage.classList.remove('hp-click-switchImg-hide');
-      }, 500);
-
-      oldImage.addEventListener('transitionend', () => {
-        oldImage.remove();
+function setupHpImageSwitchOnClick() {
+  const hpImageSwitch = document.querySelector('.hp-click-switchImg');
+  if (hpImageSwitch) {
+    hpImageSwitch.addEventListener('click', function() {
+      document.querySelectorAll('.dp-card-img-wrapper').forEach(container => {
+        const oldImage = container.querySelector('.hp-click-switchImg-animation');
+        if (oldImage) {
+          const newImage = document.createElement('img');
+          newImage.src = 'images/sapin-sans-epine.png';
+          newImage.alt = 'Nouvelle Image';
+          newImage.classList.add('hp-click-switchImg-animation', 'hp-click-switchImg-hide');
+          container.appendChild(newImage);
+          setTimeout(() => {
+            oldImage.classList.add('hp-click-switchImg-hide');
+            newImage.classList.remove('hp-click-switchImg-hide');
+          }, 500);
+          oldImage.addEventListener('transitionend', () => {
+            oldImage.remove();
+          });
+        }
       });
     });
-  });
-
-  // Header : tooltips
-
-  document.getElementById('logoContainer').addEventListener('mouseover', function () {
-    const tooltip = this.querySelector('.tooltip');
-    tooltip.style.display = tooltip.style.display === 'none' ? 'block' : 'none';
-  });
-
-  // HP: modal d'entree
-
-  // Ouvrir la modale au chargement de la page
-  window.onload = function () {
-    document.getElementById('myModal').style.display = "block";
   }
+}
 
-  // Fermer la modale lorsque l'utilisateur clique sur le symbole (×)
-  document.getElementsByClassName('close')[0].addEventListener('click', function () {
-    document.getElementById('myModal').style.display = "none";
-  });
-
-    // Fermer la modale lorsque l'utilisateur clique sur le symbole (×)
-    document.getElementsByClassName('agree')[0].addEventListener('click', function () {
-      document.getElementById('myModal').style.display = "none";
+function setupHeaderTooltips() {
+  const logoContainer = document.getElementById('logoContainer');
+  if (logoContainer) {
+    logoContainer.addEventListener('mouseover', function() {
+      const tooltip = this.querySelector('.tooltip');
+      if (tooltip) {
+        tooltip.style.display = tooltip.style.display === 'none' ? 'block' : 'none';
+      }
     });
-  
+  }
+}
 
+function setupModalEntry() {
+  const modal = document.getElementById('myModal');
+  if (modal) {
+    window.onload = function () {
+      modal.style.display = "block";
+    }
+
+    const closeModalElements = document.querySelectorAll('.close, .agree');
+    closeModalElements.forEach(element => {
+      element.addEventListener('click', function () {
+        modal.style.display = "none";
+      });
+    });
+  }
+}
+
+function setupCamionPaiementClick() {
+  const sidebar = document.getElementById("animSidebar");
+  const camionPaiement = document.querySelectorAll(".barre-avancement-camion");
+  if (sidebar) {
+    camionPaiement.forEach(element => {
+      element.addEventListener('click', function () {
+        this.classList.add('anim-rotate');
+        sidebar.classList.add('anim-rotate-bar');
+      });
+    });
+  }
+}
+
+function setupPaiementClickChoice() {
+  const btnRadioElements = [
+    { radio: 'radio1', parent: 'btn1' },
+    { radio: 'radio2', parent: 'btn2' },
+    { radio: 'radio3', parent: 'btn3' },
+  ];
+
+  btnRadioElements.forEach(({ radio, parent }) => {
+    const btnRadio = document.getElementById(radio);
+    const parentBtnRadio = document.getElementById(parent);
+
+    if (btnRadio && parentBtnRadio) {
+      btnRadio.addEventListener('change', function () {
+        btnRadioElements.forEach(({ parent }) => {
+          const parentElement = document.getElementById(parent);
+          if (parentElement) {
+            parentElement.classList.remove('inputChecked');
+          }
+        });
+        parentBtnRadio.classList.add('inputChecked');
+      });
+
+      // Au chargement initial de la page
+      if (btnRadio.checked) {
+        parentBtnRadio.classList.add('inputChecked');
+      }
+    }
+  });
+}
+
+function animationInit() {
+  setupTitleHangAnimation();
+  setupTitleFalldownAnimation();
+  setupAvisFalldownAnimation();
+  setupParagrapheFalldownAnimation();
+  setupImageSwitchOnClick();
+  setupHeaderButtonExplosion();
+  setupShootingStarsAnimation();
+  setupProductCardHangAnimation();
+  setupHpImageSwitchOnClick();
+  setupHeaderTooltips();
+  setupModalEntry();
+  setupCamionPaiementClick();
+  setupPaiementClickChoice();
 }
 
 export { animationInit };
